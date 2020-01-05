065e98e287458a55b3fe823b7126f3886ed2868b
Mykhaylo Herasika <mykhaylo.herasika@globaltechmakers.com>
2020-01-05 09:45:59 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace my_mvc_demo_react
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>().UseUrls("http://*:" + port); ;
+
+        }
+
+    }
+}
